abb736392cfb023653541c43c752b41ca7be1d60
Sina Rezaeizadeh <sina.rezaeizadeh@mail.utoronto.ca>
2016-11-19 14:04:44 -0500

    MA GUCCI

--- hack-with-ix/api/dataGenerator.js
+++ hack-with-ix/api/dataGenerator.js
@@ -1,12 +1,17 @@
 //var random = require('random-js');
 
-var my_data = []
 
-for (let i = 0; i < 20; i++){
-	let x = {
-		timestamp: Math.floor(Math.random()*10+1),
-		spend: Math.random()*10+1
+var my_data = [];
+
+var max = 10;
+var min = 0;
+for (var i = 0; i < 40; i++){
+	var x = {
+		timestamp: Math.floor(Math.random() *(max - min + 1)) + min,
+		spend: Math.random() *(max - min + 1) + min
 	}
+	min = max;
+	max += 10;
 	my_data.push(x);
 	console.log(x);
 }
