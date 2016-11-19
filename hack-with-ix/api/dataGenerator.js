c40a1ef44da3260dc0761f93a02a3898a8faabce
ousmane.amadou <ousmane.amadou89@gmail.com>
2016-11-19 14:08:38 -0500

    rip

--- hack-with-ix/api/dataGenerator.js
+++ hack-with-ix/api/dataGenerator.js
@@ -2,11 +2,10 @@
 
 var my_data = []
 
-for (let i = 0; i < 20; i++){
-	let x = {
+for (var i = 0; i < 20; i++){
+	var x = {
 		timestamp: Math.floor(Math.random()*10+1),
 		spend: Math.random()*10+1
 	}
 	my_data.push(x);
-	console.log(x);
 }
